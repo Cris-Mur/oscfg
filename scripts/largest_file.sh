9005327d69f3d89a55fe2bf2793582077cc8be9c
Cristian Murcia <cristian.murcia@enmedio.co>
2022-11-23 14:48:07 -0500

    [ update ][ argument cheker ]

--- scripts/largest_file.sh
+++ scripts/largest_file.sh
@@ -2,12 +2,15 @@
 if [ $# -eq 0 ]
 then
     echo "Usage: largest_file [Path to evaluate] [Number of files that you list]"
+    exit
 fi
 
+NLINES=20
 if [ -z "$2" ]
 then
     echo 'default output 20 Lines'
-    $2 = 20
+else
+    NLINES="$2"
 fi
 
-sudo du -aBm $1 2>/dev/null | sort -nr | head -n $2
+du -aBm $1 2>/dev/null | sort -nr | head -n $NLINES 
